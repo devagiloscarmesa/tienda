ad3a276a866b815f0192d259c17f97c4a1166846
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-05-27 20:05:50 -0500

    Updates

---
+++ precache-manifest.1d0a0f14537f8f6d0e70d538d3f3dde3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5882282eb779af83442821bcf8e3b5",
+    "url": "/tienda/index.html"
+  },
+  {
+    "revision": "583291775289f1d51705",
+    "url": "/tienda/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3c56cf821058cd43824b",
+    "url": "/tienda/static/css/main.bd28fbaf.chunk.css"
+  },
+  {
+    "revision": "583291775289f1d51705",
+    "url": "/tienda/static/js/2.24af0ba9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tienda/static/js/2.24af0ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c56cf821058cd43824b",
+    "url": "/tienda/static/js/main.33ecdb71.chunk.js"
+  },
+  {
+    "revision": "659b6b654964d4a00780",
+    "url": "/tienda/static/js/runtime-main.710d0bf9.js"
+  }
+]);
